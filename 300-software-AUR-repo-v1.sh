8ad2f26dadaf853492b3f6b983dc285c36f6e479
Erik Dubois <erik.dubois@gmail.com>
2018-05-20 09:16:15 +0200

    xcursor out because of conflict

--- 300-software-AUR-repo-v1.sh
+++ 300-software-AUR-repo-v1.sh
@@ -86,7 +86,7 @@ sh AUR/install-surfn-icons-git-v*.sh
 sh AUR/install-ttf-font-awesome-v*.sh
 sh AUR/install-ttf-mac-fonts-v*.sh
 sh AUR/install-virtualbox-for-linux-v*.sh
-sh AUR/install-xcursor-breeze-v*.sh
+#sh AUR/install-xcursor-breeze-v*.sh
 
 #Other possibilities
 #sh AUR/install-canon-mg7750-v*.sh
