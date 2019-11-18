bf6731d1f9aa8cecb15dd0797c845bea710b1998
Erik Dubois <erik.dubois@gmail.com>
2019-11-18 10:37:59 +0100

    no ncmpcpp

--- 400-software-arch-linux-repo-distro-specific-v1.sh
+++ 400-software-arch-linux-repo-distro-specific-v1.sh
@@ -36,7 +36,7 @@ echo "Installing category Internet"
 
 echo "Installing category Multimedia"
 
-sh AUR/install-mpd-ncmpcpp-v*.sh
+#sh AUR/install-mpd-ncmpcpp-v*.sh
 
 echo "Installing category Office"
 
